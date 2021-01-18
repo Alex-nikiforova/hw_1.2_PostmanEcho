69bb44df8314263cabec2265cd2e5e3b917c7cc3
Alexandra Nikiforova <sunny.alej@gmail.com>
2021-01-19 00:43:31 +0300

    add test

---
+++ src/test/java/ru/netology/PostmanEchoTest.java
@@ -0,0 +1,28 @@
+package ru.netology;
+
+import io.restassured.http.ContentType;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static io.restassured.RestAssured.urlEncodingEnabled;
+import static org.hamcrest.Matchers.*;
+
+public class PostmanEchoTest {
+
+    @Test
+    public void shouldPostToPostmanEcho() {
+        given()
+                .baseUri("https://postman-echo.com")
+                .body("Hello World!")
+        .when()
+                .post("/post")
+        .then()
+                .statusCode(200)
+                .contentType(ContentType.JSON)
+                .body("data", equalTo("Hello World!!!"))
+                .body("headers.content-length", equalTo("12"))
+                .body("headers.user-agent", equalTo("Apache-HttpClient/4.5.3 (Java/11.0.9)"))
+                .body("headers.accept", equalTo("*/*"))
+                .body("headers.accept-encoding", equalTo("gzip,deflate"));
+    }
+}
